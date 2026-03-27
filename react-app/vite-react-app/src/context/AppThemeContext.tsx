d6530bfdfa6ce26f1aab405d6055d393cde8a560
AyushiNayak <30399022+AyushiNayak@users.noreply.github.com>
2026-03-27 17:39:00 +0530

    commit

---
+++ react-app/vite-react-app/src/context/AppThemeContext.tsx
@@ -0,0 +1,23 @@
+'use client';
+import React, { type JSX, useState } from "react";
+
+export type ThemeState ={
+    mode:string;
+    changeMode: (mode:string) => void;
+}
+const initialState: ThemeState = {
+    mode: "light",
+    changeMode: (mode) => {
+
+    }
+}
+export const AppThemeContext = React.createContext<ThemeState>(initialState);
+
+export function AppThemeProvider({children}: {children: JSX.Element}) {
+    const [mode, setMode] = useState<string>(initialState.mode);
+    return (
+        <AppThemeContext.Provider value={{ mode, changeMode:setMode }}>
+            {children}
+        </AppThemeContext.Provider>
+    )
+}
